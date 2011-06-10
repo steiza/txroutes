d0287a9b2038ac5583ab902b45a3fecade5fd214
Zach Steindler <steiza@coffeehousecoders.org>
2011-06-09 20:53:32 -0400

    fixing typos

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(name='txroutes',
         author='Zach Steindler',
         author_email='steiza@coffeehousecoders.org',
         url='http://github.com/steiza/txroutes',
-        description='Provides routes-like dispatching for twisted.web.server'
+        description='Provides routes-like dispatching for twisted.web.server',
         long_description='''Frequently, it's much easier to describe your website layout using routes instead of Resource from twisted.web.resource. This small library lets you dispatch with routes in your twisted.web application.''',
         keywords='twisted, web, routes',
         classifiers=['Programming Language :: Python', 'License :: OSI Approved :: BSD License'],
         license='BSD',
         packages=['txroutes'],
         package_data={'txroutes': ['*']},
-        install_requires=['Mako'],
+        requirements=['Mako'],
         )
 
