165bd9d5338838d3fa0b8eb43516ed234ea0c5e4
Zach Steindler <steiza@coffeehousecoders.org>
2013-12-11 14:51:55 -0500

    bumping version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='txroutes',
-        version='0.0.4',
+        version='0.0.6',
         author='Olark',
         url='http://github.com/olark/txroutes',
         description='Provides routes-like dispatching for twisted.web.server',
