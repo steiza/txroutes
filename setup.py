a867d37d4a4a5bde668efaac8ce62fcb66338249
Zach Steindler <steiza@coffeehousecoders.org>
2013-12-11 14:48:21 -0500

    switching ownership to Olark

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
 setup(name='txroutes',
-        version='0.0.3',
-        author='Zach Steindler',
-        author_email='steiza@coffeehousecoders.org',
-        url='http://github.com/steiza/txroutes',
+        version='0.0.4',
+        author='Olark',
+        url='http://github.com/olark/txroutes',
         description='Provides routes-like dispatching for twisted.web.server',
         long_description='''Frequently, it's much easier to describe your website layout using routes instead of Resource from twisted.web.resource. This small library lets you dispatch with routes in your twisted.web application.''',
         keywords='twisted, web, routes',
